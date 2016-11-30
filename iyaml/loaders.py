f990a959f7ca7796276b339e469709aea781a000
Miguel Angel Gordian <os.aioria@gmail.com>
2016-11-30 01:20:05 -0600

    Collect valid yaml files and theirs path

--- iyaml/loaders.py
+++ iyaml/loaders.py
@@ -10,10 +10,13 @@ except:
 
 YAML_EXTENSIONS = ['yml', 'yaml']
 
+
 def isyamlfile(path):
-    """Validate if the path refer to one yaml file"""
+    """Validate if the path refers to one yaml file"""
+
     base = '^.*\.({})$'
     extensions = '|'.join(YAML_EXTENSIONS)
+
     yml_re = re.compile(base.format(extensions))
 
     if os.path.isfile(os.path.abspath(path)) and yml_re.match(path):            return True
@@ -21,13 +24,22 @@ def isyamlfile(path):
     return False
 
 
-def seed():
-    for root, dirs, files in os.walk(i18N_PATH):
+def collect_resources(yaml_dir='locales'):
+    """Build a base dict with all valid yaml files"""
+    roots = {}
+
+    for root, dirs, files in os.walk(yaml_dir):
         try:
-            yaml_files = filter(lambda file: file, files)
+            yaml_files = [yf for yf in files
+                          if isyamlfile(os.path.join(root, yf))]
+
+            if len(yaml_files) > 0:
+                roots[root] = yaml_files
         except FileNotFoundError as e:
             pass
 
+    return roots
 
-def transform():
+
+def transform(roots, storage_driver):
     pass
--- tests/test_loaders.py
+++ tests/test_loaders.py
@@ -8,9 +8,22 @@ class TestRawResources(unittest.TestCase):
 
     def test_if_non_existing_file_is_a_valid_yaml_file(self):
         self.assertFalse(l.isyamlfile("random.yml"))
-    
+
     def test_if_one_yaml_file_resource_is_a_valid_yaml_file(self):
         self.assertTrue(l.isyamlfile('examples/basic.yml'))
+
+
+
+class TestFileCollector(unittest.TestCase):
+    def test_if_directory_no_collect_yaml_files(self):
+        self.assertEqual(l.collect_resources('test'), {})
+
+        self.assertEqual(l.collect_resources('/tmp'), {})
+
+
+    def test_if_directory_collect_yaml_files(self):
+        self.assertEqual(len(l.collect_resources('examples').keys()), 1)
+        self.assertEqual(len(l.collect_resources('examples').values()), 1)
 
 
 if __name__ == '__main__':
